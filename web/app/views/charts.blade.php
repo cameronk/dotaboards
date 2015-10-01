3c7257c3382b90bff7078ef80f77f68a708f5d97
Cameron Kelley <cameron@azuru.me>
2015-10-01 18:31:39 -0500

    Fixed html typo

--- web/app/views/charts.blade.php
+++ web/app/views/charts.blade.php
@@ -135,7 +135,7 @@
         }
     };
     var fullDate = function(date) {
-    	return "<strong>"{ 0: "Sun",  1: "Mon", 2: "Tues", 3: "Wed", 4: "Thurs", 5: "Fri", 6: "Sat" }[date.getDay()] + "</strong><br/>" + date.getDate() + " " + { 0: "Jan", 1: "Feb", 2: "Mar", 3: "Apr", 4: "May", 5: "Jun", 6: "Jul", 7: "Aug", 8: "Sep", 9: "Oct", 10: "Nov", 11: "Dec" }[date.getMonth()];
+    	return "<strong>" + { 0: "Sun",  1: "Mon", 2: "Tues", 3: "Wed", 4: "Thurs", 5: "Fri", 6: "Sat" }[date.getDay()] + "</strong><br/>" + date.getDate() + " " + { 0: "Jan", 1: "Feb", 2: "Mar", 3: "Apr", 4: "May", 5: "Jun", 6: "Jul", 7: "Aug", 8: "Sep", 9: "Oct", 10: "Nov", 11: "Dec" }[date.getMonth()];
     };
 
 	var delays = [];
